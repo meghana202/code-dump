0baeda8ee6c229cea89ece26c19086fd4029237b
meghana202 <meghymail@gmail.com>
2021-08-08 12:06:06 -0700

    whois.com test

---
+++ selenium/test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+
+def Hello():
+	driver = webdriver.Chrome()
+	driver.get("https://www.whois.com/whois")
+ 
+	search_box = driver.find_element_by_css_selector('#query')  
+	search_box.send_keys('www.google.com')               
+	
+	submit = driver.find_element_by_css_selector('#page-wrapper > div.whois-masthead > form > button')
+	submit.click()
+	driver.close()
+
+
+Hello()
+
+
