208b3284f9eb66653b63002068ad709ef9c2ecdd
meghana202 <meghymail@gmail.com>
2021-08-08 12:30:22 -0700

    expire, registered

--- selenium/test.py
+++ selenium/test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+import time
 
 
 def test():
@@ -12,9 +13,15 @@ def test():
 	
 	submit = driver.find_element_by_css_selector('#page-wrapper > div.whois-masthead > form > button')
 	submit.click()
+	
+	register = driver.find_element_by_css_selector('#page-wrapper > div > div.whois_main_column > div:nth-child(6) > div:nth-child(5) > div.df-value')
+	print(f"Registered On: {register.text}")
+	
+	expire = driver.find_element_by_css_selector('#page-wrapper > div > div.whois_main_column > div:nth-child(6) > div:nth-child(5) > div.df-value')
+	print(f"Expires On: {expire.text}")
+	
+	time.sleep(10)
 	driver.quit()
-
-
 test()
 
 
